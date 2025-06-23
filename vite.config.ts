9809a422200366027ca5a550f2c0649b8e3749eb
Your Name <you@example.com>
2025-06-23 19:14:34 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-
-
-
   server: {
     host: "::",
     port: 8080,
-  },
-  base: '/Portfolio/',
-  build: {
-    outDir: 'dist' // Ensure output is 'dist'
   },
   plugins: [
     react(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base:"/Bharath-Portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
