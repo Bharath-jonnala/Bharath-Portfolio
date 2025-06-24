0d5e82e2be39a2530a2583941f3f419e15fec459
Your Name <you@example.com>
2025-06-24 20:09:45 +0530

    vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,21 @@
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development'&&
-  ].filter(Boolean),
-  base:"/Bharath-Portfolio",
+export default defineConfig(() => ({
+  base: "/Bharath-Portfolio/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  
+  server: {
+    host: "::",
+    port: 8080,
+  },
 }));
+
